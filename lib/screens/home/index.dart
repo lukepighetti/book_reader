6ceec3bf18bb962fa3935db622d989a72874524e
Luke Pighetti <lukepighetti@gmail.com>
2018-08-29 10:41:22 -0400

    attribution

--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -26,13 +26,25 @@ class _Body extends StatelessWidget {
 class _Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/overlay.png"),
-          fit: BoxFit.cover,
+    return Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/overlay.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Container(
+          padding: EdgeInsets.only(bottom: 36.0),
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            "BY LUKE PIGHETTI",
+            style: TextStyle(fontSize: 10.0, color: Colors.white),
+          ),
+        )
+      ],
     );
   }
 }
